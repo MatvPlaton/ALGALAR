79c0aed7368e0e5f2bf36f821a35822578b0829c
MatvPlaton <platonovmatvej0@gmail.com>
2025-01-21 02:20:21 +0300

    redux persist fix

--- src/app/redux/ReduxProvider.tsx
+++ src/app/redux/ReduxProvider.tsx
@@ -1,15 +1,15 @@
-'use client';
+// redux/ReduxProvider.tsx
+'use client'; // Required for client-side state management
 
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store';
+import store from './store';
 
-export default function ReduxProvider({ children }: { children: React.ReactNode }) {
-    return (
-        <Provider store={store}>
-            <PersistGate persistor={persistor}>
-                {children}
-            </PersistGate>
-        </Provider>
-    );
+interface ReduxProviderProps {
+    children: React.ReactNode;
 }
+
+const ReduxProvider = ({ children }: ReduxProviderProps) => {
+    return <Provider store={store}>{children}</Provider>;
+};
+
+export default ReduxProvider;
--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,42 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { combineReducers } from 'redux';
-import authSlice from './authSlice'; // Your slices
+import authSlice from './authSlice';
 import passwordSlice from './passwordSlice';
 import emailSlice from "./emailSlice";
 import innSlice from "./innSlice";
 import refreshSlice from "./refreshSlice";
-// Persist configuration
-const persistConfig = {
-    key: 'root',
-    storage,
-};
 
-// Combine reducers
-const rootReducer = combineReducers({
+// Create the store
+const store = configureStore({
+    reducer: {
     auth: authSlice,
     password: passwordSlice,
     email: emailSlice,
     inn: innSlice,
     refresh: refreshSlice
+    }
 });
 
-// Persist reducer
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// Create the store
-export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: false, // Disable checks for redux-persist
-        }),
-});
-
-export const persistor = persistStore(store);
 
 // Infer types for state and dispatch
+export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
