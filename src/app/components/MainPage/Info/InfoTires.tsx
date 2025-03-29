3c5053acb3be30a88c8ef86ec4f24dec5717e627
MatvPlaton <platonovmatvej0@gmail.com>
2025-03-29 14:00:30 +0300

    mainPage scalablity update

--- src/app/components/MainPage/Info/InfoTires.tsx
+++ src/app/components/MainPage/Info/InfoTires.tsx
@@ -8,12 +8,13 @@ const InfoTires = () => {
     max-[1281px]:top-[80%]
     max-[961px]:flex-col max-[961px]:left-[28%] max-[961px]:w-3/5 max-[961px]:top-[67%]
     max-[769px]:top-[71%]
-    max-[641px]:top-[77%]
+    max-[641px]:top-[71%]
 
-    max-[481px]:top-[99%]">
+    max-[481px]:top-[81%]
+    max-[385px]:top-[83%]">
         <div className="relative h-0 left-[2.85%] top-[20%] text-white text-right transform translate-x-[-20%] 
         text-[2rem] leading-[2rem] font-[MontserratSemiBold] max-[1281px]:top-[0] 
-         max-[961px]:text-center max-[641px]:top-[-23%]">
+         max-[961px]:text-center max-[641px]:top-[25%]">
           Отсутствие постоянного контроля <br />
           давления в шинах - это прямая <br />
           угроза безопасности вашего <br />
@@ -22,13 +23,15 @@ const InfoTires = () => {
 
         <Image
           className=" relative top-[6%] max-w-[40vw] max-h-[30vw]  w-[20rem] h-[15rem]
-          max-[961px]:top-[65%] max-[961px]:left-[3%] max-[769px]:top-[76%]"
+          max-[961px]:top-[65%] max-[961px]:left-[3%] max-[769px]:top-[76%]
+          max-[641px]:top-[121%] max-[481px]:top-[142%]"
           src={BadTyre1}
           alt=""
         />
         <Image
           className=" relative top-[6%] max-w-[40vw] max-h-[30vw] left-[10%] w-[20rem] h-[15rem]
-          max-[961px]:top-[85%] max-[961px]:left-[3%]"
+          max-[961px]:top-[85%] max-[961px]:left-[3%] max-[641px]:top-[131%]
+          max-[481px]:top-[152%] max-[385px]:top-[148%]"
           src={BadTyre2}
           alt=""
         />
--- src/app/components/MainPage/Info/PurpleBlock.tsx
+++ src/app/components/MainPage/Info/PurpleBlock.tsx
@@ -9,7 +9,8 @@ const PurpleBlock = () => {
     max-[769px]:top-[54%] max-[769px]:left-1/10 max-[769px]:w-8/10
     max-[769px]:h-[16%]
     max-[641px]:h-[18%]
-    max-[481px]:top-[76%] max-[481px]:h-[22%]">
+    max-[481px]:top-[65%] max-[481px]:h-[17.5%]
+    max-[385px]:h-[19.5%]">
 
         <div className="relative left-[12%] text-left text-[1.7rem] w-4/5 top-[2%]">
          
