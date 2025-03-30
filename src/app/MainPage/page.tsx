3345fe5c8734cf9bb2657aadb3051612c1024b08
MatvPlaton <platonovmatvej0@gmail.com>
2025-03-30 14:40:20 +0300

    some fixes

--- src/app/MainPage/page.tsx
+++ src/app/MainPage/page.tsx
@@ -4,13 +4,11 @@ import Truck from '@/app/components/MainPage/Truck';
 import Info from '@/app/components/MainPage/Info';
 import Tires from '../components/MainPage/Tires';
 import Footer from '../components/MainPage/Footer';
-import B from '../components/MainPage/B';
 const Page = () => {
 
   return (
     <>
       <Header />
-      <B />
       <Truck />
       <Info />
       <Tires />
